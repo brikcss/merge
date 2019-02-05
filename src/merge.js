9d8902c59c39f9a3cfb43a28062deb0f670e2ddc
Zimmee <thezimmee@gmail.com>
2019-02-05 10:21:40 -0700

    feature(api): Create `Merge.objects and Merge.arrays` methods.
    [BRANCH: master]

--- src/merge.js
+++ src/merge.js
@@ -1,132 +1,139 @@
-/** ------------------------------------------------------------------------------------------------
- *  @filename  merge.js
- *  @author  brikcss  <https://github.com/brikcss>
- *  @description  Deep merge objects and arrays.
- ** --------------------------------------------------------------------------------------------- */
+/*! merge.js | @author brikcss <https://github.com/brikcss> | @reference https://github.com/brikcss/merge */
+
+// -------------------------------------------------------------------------------------------------
+// Merge constructor.
+//
 
 /**
- *  Merge arrays according to a given strategy.
- *
- *  @param   {Array}  target  Target Array.
- *  @param   {Array}  source  Source Array.
- *  @param   {String}  'unique' ('unique'|'overwrite'|'concat')  Strategy to merge arrays with.
- *      'unique' concatenates and removes duplicate items. 'concat' combines source and target.
- *      'overwrite' takes source values.
- *  @return  {Array}  Merged array.
+ * Merge constructor. Provides a consistent API container for merging Objects or Arrays.
+ * @return {Object|Array}  Merged Object or Array.
  */
-function mergeArray(target = [], source, { arrayStrategy = 'unique' } = {}) {
-	// Make sure target is an array.
-	if (!(target instanceof Array)) target = [target];
-	// Merge according to array strategy.
-	if (arrayStrategy === 'overwrite') {
-		target = source;
-	} else if (arrayStrategy === 'unique') {
-		target = target.concat(source).filter((item, i, array) => array.indexOf(item) === i);
-	} else if (arrayStrategy === 'concat') {
-		target = target.concat(source);
-	} else {
-		target = source;
-	}
-	return target;
+function Merge (...args) {
+  let options = {}
+  let objects = args
+
+  // If there are two args and first is an Array and second is an Object, use Array as objects and
+  // Object as options.
+  if (objects.length === 2 && isArray(objects[0]) && isObject(objects[1])) {
+    options = objects[1]
+    objects = objects[0]
+  }
+
+  // Validate there is at least two items in objectsArray and they are all mergeable.
+  if (objects.length < 1) return false
+  if (objects.length < 2) return objects[0]
+
+  // Set target and remove target from objectsArray.
+  let target = objects.shift()
+
+  // Reduce all object in array and merge them all.
+  return objects.reduce((target, source) => {
+    // If source is not mergeable, return the source.
+    if (!mergeable(source)) return source
+    // Merge source/target as Arrays or Objects.
+    return Merge[isArray(source) ? 'arrays' : 'objects'](target, source, options)
+  }, target)
 }
 
 /**
- *  Deep merge two objects.
- *
- *  @param   {Object}  target  Target object.
- *  @param   {Object}  source  Source object.
- *  @param   {Object}  options  Options.
- *  @return  {Object}  Merged object.
+ * Merge two Arrays according to the specified strategy.
+ * @param  {Array}  target
+ * @param  {Array} source
+ * @param  {String} options.arrayStrategy  ('unique')  Strategy to merge Arrays with. 'concat'
+ *     concatenates. 'unique' concatenates and removes duplicates. 'overwrite' overwrites target
+ *     with source Array.
+ * @return {Array}  Merged Arrays.
  */
-function mergeObject(target = {}, source, options = {}) {
-	// If both are mergeable and of the same object type, merge them.
-	Object.keys(source).forEach(function(key) {
-		const value = source[key];
-
-		if (value === target) {
-			// Return target if source and target are the same.
-			return;
-		} else if (
-			!(value instanceof Object) ||
-			(value.constructor !== Object && value.constructor !== Array)
-		) {
-			// If source is not mergeable, take the source.
-			target[key] = value;
-			return;
-		} else if (value instanceof Array) {
-			// If source is an array, merge as an array.
-			target[key] = mergeArray(target[key], value, options);
-			return;
-		} else if (
-			!(target[key] instanceof Object) ||
-			(target[key].constructor !== Object && target[key].constructor !== Array)
-		) {
-			// If source is mergeable and target is not, clone it.
-			if (value instanceof Array) {
-				target[key] = mergeArray([], value, options);
-			} else {
-				target[key] = mergeObject({}, value, options);
-			}
-			return;
-		} else {
-			// Otherwise, merge as an object.
-			target[key] = mergeObject(target[key], value, options);
-			return;
-		}
-	});
-	return target;
+Merge.arrays = (target = [], source, { arrayStrategy = 'unique' } = {}) => {
+  // If source or target are not Arrays, return target.
+  if (!isArray(target) || !isArray(source)) return target
+  // Merge according to array strategy.
+  if (arrayStrategy === 'overwrite') {
+    target = source
+  } else if (arrayStrategy === 'unique') {
+    target = target.concat(source).filter((item, i, array) => array.indexOf(item) === i)
+  } else if (arrayStrategy === 'concat') {
+    target = target.concat(source)
+  } else {
+    target = source
+  }
+  return target
 }
 
 /**
- *  Deep merge two objects.
- *
- *  @param   {Object|Array}  target  Target object/array.
- *  @param   {Object|Array}  source  Source object/array.
- *  @param   {Object}  options  Options.
- *  @return  {Object|Array}  Merged object or array.
+ * Deep merge two objects.
+ * @param  {Object} target  Target Object.
+ * @param  {Object} source  Source Object.
+ * @param  {Object} options
+ * @return {Object}  Merged Objects.
  */
-function mergeTwoObjects(target, source, options = {}) {
-	// If source is not mergeable, return the source.
-	if (
-		!(source instanceof Object) ||
-		(source.constructor !== Object && source.constructor !== Array)
-	) {
-		return source;
-	}
-	// Merge according as an object or array...
-	if (source instanceof Array) target = mergeArray(target, source, options);
-	else target = mergeObject(target, source, options);
-	return target;
+Merge.objects = (target = {}, source, options = {}) => {
+  // If both are mergeable and of the same object type, merge them.
+  Object.keys(source).forEach(function (key) {
+    const value = source[key]
+    // Return target if source and target are the same.
+    if (value === target[key] || !exists(value)) return
+    // If source is not mergeable, overwrite the target.
+    if (!mergeable(value)) {
+      target[key] = value
+    // If source is an array, merge as an array.
+    } else if (isArray(value)) {
+      // If target is not an Array, convert it to one according to arrayStrategy.
+      if (!isArray(target[key])) {
+        if (options.arrayStrategy === 'overwrite') target[key] = []
+        else target[key] = exists(target[key]) ? [target[key]] : []
+      }
+      target[key] = Merge.arrays(target[key], value, options)
+    // Otherwise, merge as an object.
+    } else {
+      target[key] = Merge.objects(isObject(target[key]) ? target[key] : {}, value, options)
+    }
+  })
+  return target
+}
+
+// -------------------------------------------------------------------------------------------------
+// Helper functions.
+//
+
+/**
+ * Test if value is a true Object.
+ * @param  {any}  value
+ * @return {Boolean}  Whether value is a true Object.
+ */
+function isObject (value) {
+  return value && typeof value === 'object' && value.constructor && value.constructor === Object
 }
 
 /**
- *  Entry which handles how to merge the given objects. If the first argument is not an Array, or
- *  the second argument is an Array, all arguments are treated as objects to be merged (with the
- *  default options). This allows user to pass arguments directly to function, unless options are
- *  needed.
- *
- *  @param   {Array}  objectsArray  Array of objects.
- *  @param   {Object}  options  Options object.
- *  @return  {Object|Array}  Merged object or array.
+ * Test if value is an Array.
+ * @param  {any}  value
+ * @return {Boolean}  Whether value is an Array.
  */
-function merge(objectsArray, options = {}) {
-	// If the first argument is NOT an array, objectsArray = arguments as it is assumed there are no options being passed.
-	if (!(objectsArray instanceof Array) || arguments[1] instanceof Array) {
-		objectsArray = Array.prototype.slice.call(arguments, 0);
-	}
-
-	// Validate there is at least two items in objectsArray and they are all mergeable.
-	if (objectsArray.length < 1) return false;
-	if (objectsArray.length < 2) return objectsArray[0];
-
-	// Set target and remove target from objectsArray.
-	let target = objectsArray[0];
-	objectsArray = objectsArray.slice(1);
-
-	// Reduce all object in array and merge them all.
-	objectsArray.forEach((obj) => (target = mergeTwoObjects(target, obj, options)));
-
-	return target;
+function isArray (value) {
+  return value instanceof Array
 }
 
-export default merge;
+/**
+ * Test if value exists.
+ * @param  {any} value
+ * @return {Boolean}  Whether value exists.
+ */
+function exists (value) {
+  return value !== null && value !== undefined
+}
+
+/**
+ * Test if a value is mergeable (Object or Array).
+ * @param  {any} value
+ * @return {Boolean}  Whether value is mergeable.
+ */
+function mergeable (value) {
+  return isArray(value) || isObject(value)
+}
+
+// -------------------------------------------------------------------------------------------------
+// Exports.
+//
+
+module.exports = Merge
--- test/merge.spec.js
+++ test/merge.spec.js
@@ -1,179 +1,183 @@
 /* eslint-env jest */
-const assert = require('assert');
-const merge = require('../dist/lib/merge.js');
+const assert = require('assert')
+const merge = require('../dist/lib/merge.js')
 
 describe('merge()', () => {
-	it('should merge objects', () => {
-		const test1 = [
-			{
-				a: 1,
-				b: 2,
-				d: {
-					a: 1,
-					b: [],
-					c: { test1: 123, test2: 321 }
-				},
-				f: 5,
-				g: 123,
-				i: 321,
-				j: [1, 2, 3]
-			},
-			{
-				b: 3,
-				c: 5,
-				d: {
-					b: { first: 'one', second: 'two' },
-					c: { test2: 222 }
-				},
-				e: { one: 1, two: 2 },
-				f: [],
-				g: void 0,
-				h: /abc/g,
-				i: null,
-				j: [3, 4]
-			},
-			{ b: 4, k: 'hi' }
-		];
-		const result1 = {
-			a: 1,
-			b: 4,
-			d: {
-				a: 1,
-				b: { first: 'one', second: 'two' },
-				c: { test1: 123, test2: 222 }
-			},
-			f: [5],
-			g: undefined,
-			c: 5,
-			e: { one: 1, two: 2 },
-			h: /abc/g,
-			i: null,
-			j: [1, 2, 3, 4],
-			k: 'hi'
-		};
-		assert.deepStrictEqual(merge(test1), result1);
-		assert.deepStrictEqual(merge(...test1), result1);
-	});
+  it('merge objects', () => {
+    const test1 = [
+      {
+        a: 1,
+        b: 2,
+        d: {
+          a: 1,
+          b: [],
+          c: { test1: 123, test2: 321 }
+        },
+        f: 5,
+        g: 123,
+        i: 321,
+        j: [1, 2, 3]
+      },
+      {
+        b: 3,
+        c: 5,
+        d: {
+          b: { first: 'one', second: 'two' },
+          c: { test2: 222 }
+        },
+        e: { one: 1, two: 2 },
+        f: [],
+        g: void 0,
+        h: /abc/g,
+        i: null,
+        j: [3, 4],
+        l: 3
+      },
+      { b: 4, k: 'hi', l: 0 }
+    ]
+    const result1 = {
+      a: 1,
+      b: 4,
+      d: {
+        a: 1,
+        b: { first: 'one', second: 'two' },
+        c: { test1: 123, test2: 222 }
+      },
+      f: [5],
+      g: 123,
+      c: 5,
+      e: { one: 1, two: 2 },
+      h: /abc/g,
+      i: 321,
+      j: [1, 2, 3, 4],
+      k: 'hi',
+      l: 0
+    }
+    assert.deepStrictEqual(merge({}, ...test1), result1)
+    assert.notDeepStrictEqual(test1[0], result1)
+    assert.deepStrictEqual(merge(...test1), result1)
+    assert.deepStrictEqual(test1[0], result1)
+  })
 
-	const test2 = [
-		{ a: 'a', b: 'b', c: 'c', array: [1, 2, 3] },
-		{ b: 2, one: 1, array: [2, 3, 4] },
-		{ two: 2, array: [4, 5, 6] }
-	];
-	it('should merge objects and make arrays unique', () => {
-		assert.deepStrictEqual(merge([{}, ...test2], { arrayStrategy: 'unique' }), {
-			a: 'a',
-			b: 2,
-			c: 'c',
-			one: 1,
-			two: 2,
-			array: [1, 2, 3, 4, 5, 6]
-		});
-		assert.deepStrictEqual(test2, [
-			{ a: 'a', b: 'b', c: 'c', array: [1, 2, 3] },
-			{ b: 2, one: 1, array: [2, 3, 4] },
-			{ two: 2, array: [4, 5, 6] }
-		]);
-	});
-	it('should merge objects and overwrite arrays', () => {
-		assert.deepStrictEqual(merge([{}, ...test2], { arrayStrategy: 'overwrite' }), {
-			a: 'a',
-			b: 2,
-			c: 'c',
-			one: 1,
-			two: 2,
-			array: [4, 5, 6]
-		});
-		assert.deepStrictEqual(test2, [
-			{ a: 'a', b: 'b', c: 'c', array: [1, 2, 3] },
-			{ b: 2, one: 1, array: [2, 3, 4] },
-			{ two: 2, array: [4, 5, 6] }
-		]);
-	});
-	it('should merge objects and concatenate arrays', () => {
-		assert.deepStrictEqual(merge([{}, ...test2], { arrayStrategy: 'concat' }), {
-			a: 'a',
-			b: 2,
-			c: 'c',
-			one: 1,
-			two: 2,
-			array: [1, 2, 3, 2, 3, 4, 4, 5, 6]
-		});
-		assert.deepStrictEqual(test2, [
-			{ a: 'a', b: 'b', c: 'c', array: [1, 2, 3] },
-			{ b: 2, one: 1, array: [2, 3, 4] },
-			{ two: 2, array: [4, 5, 6] }
-		]);
-	});
+  const test2 = [
+    { a: 'a', b: 'b', c: 'c', array: [1, 2, 3] },
+    { b: 2, one: 1, array: [2, 3, 4] },
+    { two: 2, array: [4, 5, 6] }
+  ]
+  it('merge objects and make arrays unique', () => {
+    assert.deepStrictEqual(merge([{}, ...test2], { arrayStrategy: 'unique' }), {
+      a: 'a',
+      b: 2,
+      c: 'c',
+      one: 1,
+      two: 2,
+      array: [1, 2, 3, 4, 5, 6]
+    })
+    assert.deepStrictEqual(test2, [
+      { a: 'a', b: 'b', c: 'c', array: [1, 2, 3] },
+      { b: 2, one: 1, array: [2, 3, 4] },
+      { two: 2, array: [4, 5, 6] }
+    ])
+  })
+  it('merge objects and overwrite arrays', () => {
+    assert.deepStrictEqual(merge([{}, ...test2], { arrayStrategy: 'overwrite' }), {
+      a: 'a',
+      b: 2,
+      c: 'c',
+      one: 1,
+      two: 2,
+      array: [4, 5, 6]
+    })
+    assert.deepStrictEqual(test2, [
+      { a: 'a', b: 'b', c: 'c', array: [1, 2, 3] },
+      { b: 2, one: 1, array: [2, 3, 4] },
+      { two: 2, array: [4, 5, 6] }
+    ])
+  })
+  it('merge objects and concatenate arrays', () => {
+    assert.deepStrictEqual(merge([{}, ...test2], { arrayStrategy: 'concat' }), {
+      a: 'a',
+      b: 2,
+      c: 'c',
+      one: 1,
+      two: 2,
+      array: [1, 2, 3, 2, 3, 4, 4, 5, 6]
+    })
+    assert.deepStrictEqual(test2, [
+      { a: 'a', b: 'b', c: 'c', array: [1, 2, 3] },
+      { b: 2, one: 1, array: [2, 3, 4] },
+      { two: 2, array: [4, 5, 6] }
+    ])
+  })
 
-	const test3 = [[1, 2, 3], [2, 3, 4, 5], [1, 5, 9, 10]];
-	it('should merge and make arrays unique', () => {
-		assert.deepStrictEqual(merge(test3, { arrayStrategy: 'unique' }), [1, 2, 3, 4, 5, 9, 10]);
-	});
-	it('should merge and concatenate arrays', () => {
-		assert.deepStrictEqual(merge(test3, { arrayStrategy: 'concat' }), [
-			1,
-			2,
-			3,
-			2,
-			3,
-			4,
-			5,
-			1,
-			5,
-			9,
-			10
-		]);
-	});
-	it('should merge and overwrite arrays', () => {
-		assert.deepStrictEqual(merge(test3, { arrayStrategy: 'overwrite' }), [1, 5, 9, 10]);
-	});
+  const test3 = [[1, 2, 3], [2, 3, 4, 5], [1, 5, 9, 10]]
+  it('merge and make arrays unique', () => {
+    assert.deepStrictEqual(merge([[], ...test3], { arrayStrategy: 'unique' }), [1, 2, 3, 4, 5, 9, 10])
+  })
+  it('merge and concatenate arrays', () => {
+    assert.deepStrictEqual(merge([[], ...test3], { arrayStrategy: 'concat' }), [
+      1,
+      2,
+      3,
+      2,
+      3,
+      4,
+      5,
+      1,
+      5,
+      9,
+      10
+    ])
+  })
+  it('merge and overwrite arrays', () => {
+    assert.deepStrictEqual(merge(test3, { arrayStrategy: 'overwrite' }), [1, 5, 9, 10])
+  })
 
-	const test4 = [
-		[2, { one: 1, two: { three: 3, four: 4 }, five: 5 }, 9, 5],
-		[1, { one: 'one', two: { three: 'three' }, four: 'four' }, 9, 10]
-	];
-	it('should merge arrays with objects with concatenate strategy', () => {
-		assert.deepStrictEqual(merge(test4, { arrayStrategy: 'concat' }), [
-			2,
-			{ one: 1, two: { three: 3, four: 4 }, five: 5 },
-			9,
-			5,
-			1,
-			{ one: 'one', two: { three: 'three' }, four: 'four' },
-			9,
-			10
-		]);
-	});
-	it('should merge arrays with objects with unique strategy', () => {
-		assert.deepStrictEqual(merge(test4, { arrayStrategy: 'unique' }), [
-			2,
-			{ one: 1, two: { three: 3, four: 4 }, five: 5 },
-			9,
-			5,
-			1,
-			{ one: 'one', two: { three: 'three' }, four: 'four' },
-			10
-		]);
-	});
-	it('should merge arrays with objects with overwrite strategy', () => {
-		assert.deepStrictEqual(merge(test4, { arrayStrategy: 'overwrite' }), test4[1]);
-	});
+  const test4 = [
+    [2, { one: 1, two: { three: 3, four: 4 }, five: 5 }, 9, 5],
+    [1, { one: 'one', two: { three: 'three' }, four: 'four' }, 9, 10]
+  ]
+  it('merge arrays with objects with concatenate strategy', () => {
+    assert.deepStrictEqual(merge([[], ...test4], { arrayStrategy: 'concat' }), [
+      2,
+      { one: 1, two: { three: 3, four: 4 }, five: 5 },
+      9,
+      5,
+      1,
+      { one: 'one', two: { three: 'three' }, four: 'four' },
+      9,
+      10
+    ])
+  })
+  it('merge arrays with objects with unique strategy', () => {
+    assert.deepStrictEqual(merge([[], ...test4], { arrayStrategy: 'unique' }), [
+      2,
+      { one: 1, two: { three: 3, four: 4 }, five: 5 },
+      9,
+      5,
+      1,
+      { one: 'one', two: { three: 'three' }, four: 'four' },
+      10
+    ])
+  })
+  it('merge arrays with objects with overwrite strategy', () => {
+    assert.deepStrictEqual(merge([[], ...test4], { arrayStrategy: 'overwrite' }), test4[1])
+  })
 
-	it('should merge two arrays, even if they are empty', () => {
-		assert.deepStrictEqual(merge([], ['one', 'two']), ['one', 'two']);
-	});
+  it('merge two arrays, even if they are empty', () => {
+    assert.deepStrictEqual(merge([], ['one', 'two']), ['one', 'two'])
+  })
 
-	it('should clone objects when the first argument is empty', () => {
-		let brik = { options: { files: ['/this/path/one', '/this/path/two'] } };
-		let child = { files: '/this/path', root: '/another/path' };
-		let merged = merge({}, brik, { options: child });
-		assert.deepStrictEqual(merged, {
-			options: {
-				files: '/this/path',
-				root: '/another/path'
-			}
-		});
-		assert.deepStrictEqual(brik, { options: { files: ['/this/path/one', '/this/path/two'] } });
-	});
-});
+  it('clone objects when the first argument is empty', () => {
+    let parent = { options: { files: ['/this/path/one', '/this/path/two'] } }
+    let child = { files: '/this/path', root: '/another/path' }
+    let merged = merge({}, parent, { options: child })
+    assert.deepStrictEqual(merged, {
+      options: {
+        files: '/this/path',
+        root: '/another/path'
+      }
+    })
+    assert.deepStrictEqual(parent, { options: { files: ['/this/path/one', '/this/path/two'] } })
+  })
+})
