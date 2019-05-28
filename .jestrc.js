852f27160f43c78cd704ee41c8b00141532fd5a3
Zimmee <thezimmee@gmail.com>
2019-05-28 12:05:05 -0600

    chore(dependencies): Update some dependencies.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	new file:   .babelrc
    #	modified:   .jestrc.js
    #	modified:   package-lock.json
    #	modified:   package.json
    #
    
    [BRANCH: master]

--- .jestrc.js
+++ .jestrc.js
@@ -1,5 +1,6 @@
-/*! .jestrc.js | @author brikcss <https://github.com/brikcss> | @reference <https://jest.io> */
+/*! .jestrc.js | @author Brikcss (https://github.com/brikcss) | @reference (https://jestjs.io) */
 
 module.exports = {
-  testMatch: ['**/test/**/*.spec.js']
+  testMatch: ['**/test/*.spec.js'],
+  collectCoverageFrom: ['src/**/*', '!src/cli.js']
 }
